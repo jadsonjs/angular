4c5ee0b217650eb6a3d4854de159a3c267cae07f
Jadson Santos <jadson@info.ufrn.br>
2017-08-30 18:26:14 -0300

    projeto helloworld angular

---
+++ helloWorldAngular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
