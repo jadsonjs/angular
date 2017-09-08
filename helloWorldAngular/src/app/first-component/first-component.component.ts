65a32ebe160c01a6da06dd6b74b71ba72cfcca3a
Jadson Santos <jadson@info.ufrn.br>
2017-09-08 16:09:59 -0300

    modules e templates
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- helloWorldAngular/src/app/first-component/first-component.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'first-component',
-    template: `<p>First Web Component in Angular</p>`
-})
-export class FirstComponent{
-
-}
